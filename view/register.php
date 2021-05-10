7d340720225c762e7899b2cd28299c27e8d9d8f3
kangbj1026 <kangbj1026@naver.com>
2021-05-10 15:38:32 +0900

    myStudy

--- view/register.php
+++ view/register.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['ss_mb_id']) && $_GET['mode'] == 'modify') { // 세션이 �
 <!DOCTYPE html>
 <html lang="ko">
 
-<head>
+<head>	
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
